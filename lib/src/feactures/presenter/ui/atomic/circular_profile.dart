d7c85190f698220411981523598bc501230aabe9
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-04-09 17:07:14 -0300

    add profile picture

---
+++ lib/src/feactures/presenter/ui/atomic/circular_profile.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:manga_easy_themes/manga_easy_themes.dart';
+
+class CircularProfile extends StatelessWidget {
+  const CircularProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      backgroundColor: ThemeService.of.backgroundColor,
+      radius: 100,
+      child: const CircleAvatar(
+        radius: 90,
+        backgroundImage: NetworkImage(
+            'https://media.discordapp.net/attachments/882615192431771668/1078880185170006056/5d138386cb0ff237d0f65cd6a9905ab4.jpg?width=576&height=650'),
+      ),
+    );
+  }
+}
--- lib/src/feactures/presenter/ui/pages/perfil_page.dart
+++ lib/src/feactures/presenter/ui/pages/perfil_page.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:manga_easy_perfil/src/feactures/presenter/ui/atomic/circular_profile.dart';
+import 'package:manga_easy_themes/manga_easy_themes.dart';
 
 class PerfilPage extends StatelessWidget {
+  static const route = '/perfil';
   const PerfilPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: ListView(
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: 200,
+                color: ThemeService.of.primaryColor,
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Icon(Icons.key, size: 35),
+                      Icon(Icons.airline_seat_recline_normal, size: 35)
+                    ],
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 50),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: CircularProfile(),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
