2066de3c116c6b1b12c9b784682afaae624c712e
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-04-10 17:54:19 -0300

    update card widget

---
+++ lib/src/feactures/presenter/ui/atomic/card_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:manga_easy_themes/manga_easy_themes.dart';
+
+class CardWidget extends StatelessWidget {
+  final Widget child;
+  final double height;
+  const CardWidget({
+    super.key,
+    required this.child,
+    required this.height,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    double paddingHorizontal = 16 * 2;
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        Container(
+          height: height,
+          width: MediaQuery.of(context).size.width - paddingHorizontal,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(
+              width: 1.5,
+              color: ThemeService.of.selectColor,
+            ),
+          ),
+          child: child,
+        ),
+        Container(
+          height: 1.5,
+          width: MediaQuery.of(context).size.width - paddingHorizontal,
+          alignment: Alignment.bottomCenter,
+          margin: const EdgeInsets.only(bottom: 5),
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1.5,
+              color: ThemeService.of.selectColor,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
