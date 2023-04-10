50c9b4007062436c7b60a234e7e25db4cffc8f1b
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-04-09 22:22:51 -0300

    description

---
+++ lib/src/feactures/presenter/ui/molecules/about_perfil.dart
@@ -0,0 +1,78 @@
+import 'package:coffee_cup/coffe_cup.dart';
+import 'package:flutter/material.dart';
+import 'package:manga_easy_perfil/src/feactures/presenter/ui/atomic/circular_profile.dart';
+import 'package:manga_easy_themes/manga_easy_themes.dart';
+
+class AboutPerfil extends StatelessWidget {
+  const AboutPerfil({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          children: [
+            Container(
+              height: 200,
+              color: ThemeService.of.primaryColor,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Icon(Icons.key, size: 35),
+                    Icon(Icons.airline_seat_recline_normal, size: 35)
+                  ],
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 50),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: CircularProfile(),
+              ),
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(
+                  width: 2,
+                  color: ThemeService.of.primaryColor,
+                ),
+              ),
+              child: CoffeeText(
+                text: 'Membro',
+                color: ThemeService.of.primaryColor,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const CoffeeText(
+              text: 'Tim maia',
+              typography: CoffeeTypography.title,
+            ),
+            const SizedBox(height: 10),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: CoffeeText(
+                text:
+                    'Ohayo Onichan!!!, Curto assistir animes e estou a procurar de um senpai para assistir animes e comentar sobre mangas comigo UwU e quem sabe coisa sapequinhas tambem ><',
+              ),
+            ),
+            const SizedBox(height: 10),
+            CoffeeText(
+              text: 'Conta criada em 20 jun, 2023',
+              color: ThemeService.of.primaryText.withOpacity(0.5),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
