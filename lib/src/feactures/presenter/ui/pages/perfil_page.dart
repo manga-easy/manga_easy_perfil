a10ccdbc9f0544cd1a5afab7e1ce94b05b7375d0
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-04-09 22:24:10 -0300

    update screen perfil

--- lib/src/feactures/presenter/ui/pages/perfil_page.dart
+++ lib/src/feactures/presenter/ui/pages/perfil_page.dart
@@ -1,5 +1,8 @@
+import 'package:coffee_cup/coffe_cup.dart';
 import 'package:flutter/material.dart';
+import 'package:manga_easy_perfil/src/feactures/presenter/ui/molecules/card_widget.dart';
 import 'package:manga_easy_perfil/src/feactures/presenter/ui/atomic/circular_profile.dart';
+import 'package:manga_easy_perfil/src/feactures/presenter/ui/molecules/about_perfil.dart';
 import 'package:manga_easy_themes/manga_easy_themes.dart';
 
 class PerfilPage extends StatelessWidget {
@@ -11,31 +14,87 @@ class PerfilPage extends StatelessWidget {
     return Scaffold(
       body: ListView(
         children: [
-          Stack(
+          const AboutPerfil(),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                height: 200,
-                color: ThemeService.of.primaryColor,
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Icon(Icons.key, size: 35),
-                      Icon(Icons.airline_seat_recline_normal, size: 35)
-                    ],
-                  ),
-                ),
+              CircleAvatar(
+                backgroundColor: ThemeService.of.selectColor,
+                radius: 50,
               ),
-              const Padding(
-                padding: EdgeInsets.only(top: 50),
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: CircularProfile(),
-                ),
+              const SizedBox(width: 20),
+              CircleAvatar(
+                backgroundColor: ThemeService.of.selectColor,
+                radius: 50,
+              ),
+              const SizedBox(width: 20),
+              CircleAvatar(
+                backgroundColor: ThemeService.of.selectColor,
+                radius: 50,
               ),
             ],
+          ),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 110),
+            child: CoffeeButton(
+              label: 'Editar perfil',
+              padding: const EdgeInsets.symmetric(vertical: 14),
+              onPress: () {},
+            ),
+          ),
+          const SizedBox(height: 20),
+          CardWidget(
+            height: 100,
+            child: Row(
+              children: [
+                Column(
+                  children: const [
+                    CoffeeText(
+                      text: 'NÍVEL',
+                      typography: CoffeeTypography.title,
+                    ),
+                    CoffeeText(
+                      text: '14',
+                      typography: CoffeeTypography.title,
+                    ),
+                  ],
+                ),
+                const SizedBox(width: 20),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 20,
+                      width: 250,
+                      child: Stack(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: ThemeService.of.selectColor,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          FractionallySizedBox(
+                            widthFactor: 0.5,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: ThemeService.of.primaryColor,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    const CoffeeText(text: '885 XP'),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
