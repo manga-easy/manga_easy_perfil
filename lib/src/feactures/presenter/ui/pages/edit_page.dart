3767859ab3738c1c49ae5cf89366c5f0a0a7bbab
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-04-11 05:09:10 -0300

    add perfil image

--- lib/src/feactures/presenter/ui/pages/edit_page.dart
+++ lib/src/feactures/presenter/ui/pages/edit_page.dart
@@ -1,4 +1,7 @@
+import 'package:coffee_cup/coffe_cup.dart';
 import 'package:flutter/material.dart';
+import 'package:manga_easy_perfil/src/feactures/presenter/ui/atomic/circle_profile.dart';
+import 'package:manga_easy_themes/manga_easy_themes.dart';
 
 class EditPage extends StatelessWidget {
   static const route = '/edit-perfil';
@@ -6,6 +9,35 @@ class EditPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ListView(
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: 125,
+                color: ThemeService.of.primaryColor,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 70),
+                child: Row(
+                  children: [
+                    CircleProfile(radius: 80),
+                    Column(
+                      children: [
+                        CoffeeButton(label: 'Alterar imagem'),
+                        CoffeeButtonText(
+                          text: 'Remover imagem',
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
